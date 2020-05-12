66d49de1b6fc9e22b6686065a8686cdffc5b5335
Tarek Khalifa <Tarekelsayedkhalifa@gmail.com>
2020-05-12 21:03:59 +0200

    Added streaming without predownloading.

--- DiscordBot/CommandHandler.py
+++ DiscordBot/CommandHandler.py
@@ -6,29 +6,30 @@ import os
 import asyncio
 from discord.ext import commands
 default_prefix = '$'
-bot = commands.Bot(command_prefix= default_prefix)
+bot = commands.Bot(command_prefix=default_prefix)
 modules = ['Modules.TextModule', 'Modules.CustomModule', 'Modules.VoiceModule']
 
 
 filepath = 'keys.txt'
 key = ''
 with open(filepath) as fp:
-   key = fp.readline()
+    key = fp.readline()
 
 prefixes = {}
 filesize = os.path.getsize("prefixes.json")
-if filesize!=0:
+if filesize != 0:
     with open('prefixes.json') as json_file:
         prefixes = json.load(json_file)
 
 
 @bot.event
-async def on_ready() :
+async def on_ready():
     print('We have logged in as {0.user}'.format(bot))
     game = discord.Game("with your feelings")
     await bot.change_presence(status=discord.Status.online, activity=game)
     for m in modules:
         bot.load_extension(m)
+
 
 @bot.event
 async def on_message(message):
@@ -36,7 +37,7 @@ async def on_message(message):
         return
     t = message.content
     t = t.split()
-    if len(t) == 2 and len(t[1])==1 and t[0] == 'prefix':
+    if len(t) == 2 and len(t[1]) == 1 and t[0] == 'prefix':
         prefix = t[1]
         prefixes[str(message.author.guild.id)] = prefix
         with open('prefixes.json', 'w') as outfile:
@@ -44,7 +45,7 @@ async def on_message(message):
         bot.command_prefix = prefix
 
     if str(message.author.guild.id) in prefixes:
-        bot.command_prefix = prefixes[str(message.author.guild.id)]   
+        bot.command_prefix = prefixes[str(message.author.guild.id)]
     else:
         bot.command_prefix = default_prefix
         prefixes[str(message.author.guild.id)] = default_prefix
@@ -53,6 +54,6 @@ async def on_message(message):
     if message.content == 'prefix':
         await message.channel.send(prefixes[str(message.author.guild.id)])
     await bot.process_commands(message)
-    
-       
+
+
 bot.run(key)
--- DiscordBot/Modules/VoiceModule.py
+++ DiscordBot/Modules/VoiceModule.py
@@ -10,9 +10,55 @@ from discord import FFmpegPCMAudio
 import youtube_dl
 from os import system
 
+youtube_dl.utils.bug_reports_message = lambda: ''
+
+
+ytdl_format_options = {
+    'format': 'bestaudio/best',
+    'outtmpl': '%(extractor)s-%(id)s-%(title)s.%(ext)s',
+    'restrictfilenames': True,
+    'noplaylist': True,
+    'nocheckcertificate': True,
+    'ignoreerrors': False,
+    'logtostderr': False,
+    'quiet': True,
+    'no_warnings': True,
+    'default_search': 'auto',
+    # bind to ipv4 since ipv6 addresses cause issues sometimes
+    'source_address': '0.0.0.0'
+}
+
+ffmpeg_options = {
+    'options': '-vn'
+}
+
+ytdl = youtube_dl.YoutubeDL(ytdl_format_options)
+
+
+class YTDLSource(discord.PCMVolumeTransformer):
+    def __init__(self, source, *, data, volume=0.5):
+        super().__init__(source, volume)
+
+        self.data = data
+
+        self.title = data.get('title')
+        self.url = data.get('url')
+
+    @classmethod
+    async def from_url(cls, url, *, loop=None, stream=False):
+        loop = loop or asyncio.get_event_loop()
+        data = await loop.run_in_executor(None, lambda: ytdl.extract_info(url, download=not stream))
+
+        if 'entries' in data:
+            # take first item from a playlist
+            data = data['entries'][0]
+
+        filename = data['url'] if stream else ytdl.prepare_filename(data)
+        return cls(discord.FFmpegPCMAudio(filename, **ffmpeg_options), data=data)
+
 
 class VoiceModule(commands.Cog):
-    def init(self, bot):
+    def __init__(self, bot):
         self.bot = bot
 
     @commands.command()
@@ -28,6 +74,17 @@ class VoiceModule(commands.Cog):
             return await ctx.voice_client.move_to(channel)
         if channel != None:
             await channel.connect()
+
+    @commands.command()
+    async def stream(self, ctx, *, url):
+        """Streams from a url (same as yt, but doesn't predownload)"""
+
+        async with ctx.typing():
+            player = await YTDLSource.from_url(url, loop=self.bot.loop, stream=True)
+            ctx.voice_client.play(player, after=lambda e: print(
+                'Player error: %s' % e) if e else None)
+
+        await ctx.send('Now playing: {}'.format(player.title))
 
     @commands.command()
     async def leave(self, ctx):
